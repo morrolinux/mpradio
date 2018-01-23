926feeecd99ecd375565787fe6961e7d7e38becd
morrolinux <morrolinux@gmail.com>
2018-01-24 00:21:04 +0100

    udev force reload is needed
    BT events can be miss if udev is not reloaded from time to time

--- install/bt-setup.sh
+++ install/bt-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#systemctl force-reload udev systemd-udevd-control.socket systemd-udevd-kernel.socket
+systemctl force-reload udev systemd-udevd-control.socket systemd-udevd-kernel.socket
 #hciconfig hci0 up
 hciconfig hci0 piscan
