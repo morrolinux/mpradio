e63ee9dbb18c85107dd8cc2fc0dc3d98bf4bbc01
morrolinux <morrolinux@gmail.com>
2018-02-01 15:47:45 +0100

    legacy rds can now be parametric

--- install/mpradio-legacyRDS.sh
+++ install/mpradio-legacyRDS.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-sleep 5
-JUMP=6
+INTERVAL=$(crudini --get /pirateradio/pirateradio.config RDS updateInterval)
+JUMP=$(crudini --get /pirateradio/pirateradio.config RDS charsJump)
+
+sleep $INTERVAL
 
 while true
 do
         title="$(cat /home/pi/now_playing)"
         title_lenght=$(echo $title|wc -c)
-
-        echo "TA ON" >/home/pi/rds_ctl
-        echo "TA $title" >/home/pi/rds_ctl
-
 	finish=$((title_lenght+JUMP)) 
 
         for i in $(seq 9 $JUMP $finish);
         do
                 legacytitle="$(echo "$title" |cut -c $(($i-8))-$i)"
                 echo "PS $legacytitle" >/home/pi/rds_ctl
-                sleep 5
+                sleep $INTERVAL
         done
 
         echo "PS $title" >/home/pi/rds_ctl
-	sleep 5
+	sleep $INTERVAL
         
 done
 
