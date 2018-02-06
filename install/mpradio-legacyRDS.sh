6675173ccf5443d1c72cc54fe7246acc89ad1457
morrolinux <morrolinux@gmail.com>
2018-02-06 19:48:01 +0100

    Improved Legacy RDS

--- install/mpradio-legacyRDS.sh
+++ install/mpradio-legacyRDS.sh
@@ -5,9 +5,14 @@ JUMP=$(crudini --get /pirateradio/pirateradio.config RDS charsJump)
 
 sleep $INTERVAL
 
+getTitle(){
+	cat /home/pi/now_playing
+}
+
 while true
 do
-        title="$(cat /home/pi/now_playing)"
+	title=$(getTitle)
+	echo $title
         title_lenght=$(echo $title|wc -c)
 	finish=$((title_lenght+JUMP)) 
 
@@ -16,10 +21,10 @@ do
                 legacytitle="$(echo "$title" |cut -c $(($i-8))-$i)"
                 echo "PS $legacytitle" >/home/pi/rds_ctl
                 sleep $INTERVAL
+		if [[ $title != $(getTitle) ]]
+		then
+			break
+		fi
         done
-
-        echo "PS $title" >/home/pi/rds_ctl
-	sleep $INTERVAL
         
 done
-
