058a45fe45154eea200af5b71bf07d9b8a362fdf
morrolinux <morrolinux@gmail.com>
2018-04-22 17:42:53 +0200

    improving read-only

--- utility/roswitch.sh
+++ utility/roswitch.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    	echo "Illegal number of parameters"
-  	exit
+        echo "Illegal number of parameters"
+        exit
 fi
 
 if [[ $1 == "ro" ]]
 then
-	sudo sed -i.bak '/^PARTUUID/ s/defaults/defaults,ro/' /etc/fstab
-	sudo sed -i.bak 's/rootwait *$/rootwait noswap ro/' /boot/cmdline.txt
-	echo "You need to reboot to make this effective."
+        sudo sed -i.bak '/^PARTUUID/ s/defaults/defaults,ro/' /etc/fstab
+        sudo sed -i.bak 's/rootwait *$/rootwait noswap ro/' /boot/cmdline.txt
+
+        if [[ $(grep "var" /etc/fstab) == "" ]]
+        then
+                echo "tmpfs /var tmpfs noatime 0 0" >> /etc/fstab
+        fi
+
+        echo "You need to reboot to make this effective."
 elif [[ $1 == "rw" ]]
 then
-	sudo mount -o remount,rw /
-	sudo mount -o remount,rw /boot
-	sudo sed -i.bak '/^PARTUUID/ s/defaults,ro/defaults/' /etc/fstab
-	sudo sed -i.bak 's/rootwait noswap ro*$/rootwait/' /boot/cmdline.txt
-	echo "RW effective now!"
+        sudo mount -o remount,rw /
+        sudo mount -o remount,rw /boot
+        sudo sed -i.bak '/^PARTUUID/ s/defaults,ro/defaults/' /etc/fstab
+        sudo sed -i.bak 's/rootwait noswap ro*$/rootwait/' /boot/cmdline.txt
+        sudo umount -l /var/
+	sed -i '/tmpfs \/var tmpfs noatime 0 0/d' /etc/fstab
+        echo "RW effective now!"
 fi
-
