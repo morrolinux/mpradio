945293dc01b27a1aacfd77371755a9dfa604f385
Owen <owen@owenashurst.com>
2018-10-27 22:39:20 +0100

    Added stationName to config. Added rdsStationName to datastruct.h Tweaked startup command to use rdsStationName as PS and songArtist and songName as RT. Added stationName to settings_provider.cc

--- src/settings_provider.cc
+++ src/settings_provider.cc
@@ -14,6 +14,7 @@ constexpr auto DEFAULTFREQ = "107.0";
 constexpr auto DEFAULTGAIN = "1.3";
 constexpr auto DEFAULTBTGAIN = "1.7";
 constexpr auto DEFAULTOUTPUT = "fm";
+constexpr auto DEFAULTSTATIONNAME = "MPRADIO";
 constexpr bool BTBOOST = true;
 constexpr int STRLEN = 40;
 
@@ -42,6 +43,7 @@ void getsettings()
 
 	s.rdsUpdateInterval=reader.GetInteger("RDS","updateInterval",4);
 	s.rdsCharsJump=reader.GetInteger("RDS","charsJump",6);
+	s.rdsStationName=reader.Get("RDS","stationName",DEFAULTSTATIONNAME);
 
 	s.storage=STORAGE;
 	s.format=reader.Get("PLAYLIST","fileFormat",FORMAT);
@@ -62,6 +64,7 @@ void getsettings()
 	cout<<"freq: "<<s.freq<<endl<<"btGain: "<<s.btGain<<" storageGain: "<<s.storageGain \
 		<<endl<<"persistentPlaylist: "<<s.persistentPlaylist<<endl \
 		<<"resumePlayback: "<<s.resumePlayback<<endl<<"shuffle: "<<s.shuffle<<endl \
+		<<"rdsStationName: "<<s.rdsStationName<<endl \
 		<<"rdsUpdateInterval: "<<s.rdsUpdateInterval<<endl<<"rdsCharsJump: " \
 		<<s.rdsCharsJump<<endl<<"Output: "<<s.output<<endl<<"Format: "\
 		<<s.format<<endl<<"implementation: "<<s.implementation<<endl<<"Treble: "<<s.treble<<endl;
